6b32264b627c15954cb14a0bf0aa1266d525ba0a
akhilvskannan77@gmail.com <akhilvskannan77@gmail.com>
2025-04-18 20:44:23 +0530

    edit profile personal tab done

---
+++ src/components/UserProfile/EditProfile/Tabs/SecurityTab.tsx
@@ -0,0 +1,109 @@
+import { FaEye, FaEyeSlash } from "react-icons/fa";
+import { useState } from "react";
+import { actionServiceUpdate } from "@/service/actionServiceUpdate";
+import { useForm } from "react-hook-form";
+import { resetPasswordSchema, signSchema } from "@/validations/authSchemas";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+
+ export function SecurityTab() {
+
+    const{ register,handleSubmit,formState:{errors,isSubmitting},watch,setValue,reset}=useForm({resolver:zodResolver(resetPasswordSchema )})
+
+    
+    const formData=watch()
+    let submit=async(e:React.FormEvent)=>{
+      // e.preventDefault()
+      console.log(watch());
+      
+      const response =await actionServiceUpdate("/api/user/profile/resetPassword",formData)
+      console.log(response);
+      
+      
+    }
+
+  return (
+    <div className="space-y-6">
+      <div>
+        <h3 className="text-lg font-medium mb-4">Password</h3>
+        <div className="space-y-4">
+          <PasswordInput
+            label="Current Password"
+            register={register}
+            type="current"
+            errors={ formData.current ? "": "*required"}
+            />
+          <PasswordInput
+            label="New Password"
+            register={register}
+            type="password"
+            errors={errors.password?.message}
+
+          />
+          <PasswordInput
+            label="Confirm New Password"
+            register={register}
+            type="confirmPassword"
+            errors={errors.confirmPassword?.message}
+            
+          />
+        </div>
+      </div>
+
+      <button
+              type="button"   onClick={handleSubmit(submit)}
+              className="px-6 py-2 bg-[#0ef] text-black font-bold rounded-md hover:bg-opacity-80 transition-opacity disabled:opacity-50"
+              disabled={isSubmitting}
+            >
+              {isSubmitting ? "Updating..." : "Upate Password"}
+            </button>
+    </div>
+  );
+}
+
+
+
+
+
+
+function PasswordInput({
+  label,
+  register,
+  type,
+  errors
+}: {
+  label: string;
+  value: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  type:string,
+  errors:string
+}) {
+  const [showPassword, setShowPassword] = useState(false);
+
+  return (
+    <div>
+      <label className="block text-gray-400 mb-2 text-sm">{label}</label>
+      <div className="relative">
+        <input
+          type={showPassword ? "text" : "password"}
+          className="w-full bg-black border border-gray-800 text-white px-3 py-2 rounded-md text-sm focus:border-[#0ef] focus:outline-none transition-colors pr-10"
+          {...register(type)}
+        />
+        {errors && <p className="text-red-500 text-xs">{errors}</p>} 
+
+        <button
+          type="button"
+          onClick={() => setShowPassword(!showPassword)}
+          className="absolute right-3 top-2.5 text-gray-400 hover:text-white transition-colors"
+        >
+          {showPassword ? <FaEyeSlash size={14} /> : <FaEye size={14} />}
+        </button>
+      </div>
+    </div>
+  );
+}
+
+
+
+
+
