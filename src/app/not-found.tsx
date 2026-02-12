e31f728939161fc23b5c06b942dd3b46130eaac6
akhilvskannan77@gmail.com <akhilvskannan77@gmail.com>
2026-02-12 13:14:17 +0530

    feat(error):not -found custom middleware added

---
+++ src/app/not-found.tsx
@@ -0,0 +1,17 @@
+
+
+
+
+
+
+
+export default function NotFound() {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <div>
+      <h1>404 – Page Not Found</h1>
+      <p>The page you are looking for does not exist.</p>
+      </div>
+    </div>
+  );
+}
